86f617d264e50d97c453c6aa6f3a40482565978b
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-04 01:02:39 +0900

    백준 입출력과 사칙연산
    10171 고양이 그리기

---
+++ Algorithm/Baekjoon/src/basic/cat.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class cat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
