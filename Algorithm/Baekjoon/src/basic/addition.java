1bf0912c5ac5cbe5cce9bd46181c71763f5a586a
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-04 01:03:22 +0900

    백준 입출력과 사칙연산
    1000 A+B

---
+++ Algorithm/Baekjoon/src/basic/addition.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Scanner;
+
+public class addition {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	    while(sc.hasNextInt()) {
+	    	int a = sc.nextInt();
+	    	int b = sc.nextInt();
+	    	System.out.println(a+b);
+	    }
+	}
+}
