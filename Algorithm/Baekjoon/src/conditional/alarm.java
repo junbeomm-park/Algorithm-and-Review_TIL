55928bd7b0648dcaf2565ef1140f521d204617e7
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-04 17:31:07 +0900

    백준 if문
    2884 알람

--- Algorithm/Baekjoon/src/conditional/alarm.java
+++ Algorithm/Baekjoon/src/conditional/alarm.java
@@ -1,10 +1,28 @@
 package conditional;
 
+import java.util.Scanner;
+
 public class alarm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int h = sc.nextInt();
+			int m = sc.nextInt();
+			
+			if(m < 45) {
+				h = h-1;
+				m = 60 - (45 - m);
+				
+				if(h < 0) {
+					h = 23;
+				}
+				System.out.println(h+" "+m);
+			} else {
+				System.out.println(h+" "+(m-45));
+			}
+		}
 	}
-
 }
+
