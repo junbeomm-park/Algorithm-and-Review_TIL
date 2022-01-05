d3f5c07af2e26e14ec4591db033fb3b8ee3d2716
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-05 21:49:28 +0900

    백준 for문
    2741 N 찍기

---
+++ Algorithm/Baekjoon/src/repetitive/output.java
@@ -0,0 +1,15 @@
+package repetitive;
+
+import java.util.Scanner;
+
+public class output {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 1 ; i <= n ; i++) {
+			System.out.println(i);
+		}
+	}
+}
