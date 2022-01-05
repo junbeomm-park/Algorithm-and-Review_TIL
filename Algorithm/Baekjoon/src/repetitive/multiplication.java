98f1b7a142945387c779fc1cf48797955642a7dd
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-05 16:48:05 +0900

    백준 for문
    2739 구구단

---
+++ Algorithm/Baekjoon/src/repetitive/multiplication.java
@@ -0,0 +1,16 @@
+package repetitive;
+
+import java.util.Scanner;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(int i = 1; i<=9; i++) {
+			System.out.println(a+"*"+i+"="+a*i);
+		}
+	}
+
+}
---
+++ Algorithm/Baekjoon/src/repetitive/submit.java
@@ -0,0 +1,16 @@
+package repetitive;
+
+import java.util.Scanner;
+
+public class submit {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		for(int i = 1 ; i <= n ; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
