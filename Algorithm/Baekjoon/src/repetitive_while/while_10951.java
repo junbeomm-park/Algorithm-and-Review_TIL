1e6648ce055b8e7d64e1c3c337d97f15701051a0
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-08 20:53:10 +0900

    백준 whilwe문
    10951 EOF

---
+++ Algorithm/Baekjoon/src/repetitive_while/while_10951.java
@@ -0,0 +1,17 @@
+package repetitive_while;
+
+import java.util.Scanner;
+
+public class while_10951 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+			
+			
+		}
+	}
+}
