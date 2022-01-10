047e302f572b26a0896fe02d70e7aafc9b286a6e
JUNBEOM PARK <junbum0674@gmail.com>
2022-01-10 20:24:15 +0900

    자바의정석 변수
    타입 간의 변환

---
+++ 자바의정석/Chapter02_Variable/src/Chapter02_Variable/Ex2_12.java
@@ -0,0 +1,13 @@
+package Chapter02_Variable;
+
+public class Ex2_12 {
+	public static void main(String[] args) {
+		String str = "3";
+		
+		System.out.println(str.charAt(0)-'0');
+		System.out.println('3' - '0' + 1);
+		System.out.println(Integer.parseInt("3")+1);
+		System.out.println("3" + 1);
+		System.out.println((char)(3 + '0'));
+	}
+}
