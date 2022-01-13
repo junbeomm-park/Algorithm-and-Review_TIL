4520387284b68ca1a019437fdc4417d0ee641c12
JUNBEOM PARK <junbum0674@gmail.com>
2022-01-13 16:19:32 +0900

    자바의정석 컬렉션 프레임워크
    Stack & Queue

---
+++ 자바의정석/The_Essence_of_Java/src/Chapter11_Collections_Framework/Ex11_Stack_Queue.java
@@ -0,0 +1,32 @@
+package Chapter11_Collections_Framework;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Ex11_Stack_Queue {
+
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		Queue q = new LinkedList();
+		
+		st.push("0");
+		st.push("1");
+		st.push("2");
+		
+		q.offer("0");
+		q.offer("1");
+		q.offer("2");
+		
+		System.out.println("== Stack ==");
+		while(!st.empty()) {
+			System.out.println(st.pop());
+		}
+		
+		System.out.println("== Queue ==");
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
+	}
+
+}
