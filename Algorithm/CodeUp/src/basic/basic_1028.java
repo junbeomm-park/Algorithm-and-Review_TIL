940b588c1eb2f1484b513583e28197c223ab8674
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-15 23:28:04 +0900

    CodeUp 기초 100제
    1028 long

---
+++ Algorithm/CodeUp/src/basic/basic_1028.java
@@ -0,0 +1,15 @@
+package basic;
+
+import java.util.Scanner;
+
+public class basic_1028 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		
+		System.out.println(a);
+		
+	}
+}
