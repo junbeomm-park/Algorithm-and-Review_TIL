02eab1f23eb3b34fdd20ad7a8d615e944edad1ea
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-15 23:34:43 +0900

    CoduUp 기초 100제
    1029 double

---
+++ Algorithm/CodeUp/src/basic/basic_1029.java
@@ -0,0 +1,15 @@
+package basic;
+
+import java.util.Scanner;
+
+public class basic_1029 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		
+		System.out.println(String.format("%.11f", a));
+		
+	}
+}
