466f1e26cebed0d5401c95d45bdaeb69086513ac
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-16 21:36:14 +0900

    백준 함수
    15596 정수 N개의 합

---
+++ Algorithm/Baekjoon/src/function/function_15596.java
@@ -0,0 +1,21 @@
+package function;
+
+import java.util.Scanner;
+
+public class function_15596 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	class Test{
+		long sum(int[] a) {
+			long sum = 0;
+			
+			for(int i=0; i<a.length; i++) {
+				sum += a[i];
+			}
+			return sum;
+		}
+	}
+}
