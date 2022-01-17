265ef94b3b4fe998c0195a28d8780daf2aa540a8
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-01-17 22:19:35 +0900

    백준 문자열
    11654 아스키코드

---
+++ Algorithm/Baekjoon/src/String/String_11654.java
@@ -0,0 +1,16 @@
+package String;
+
+import java.util.Scanner;
+
+public class String_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char a = sc.nextLine().charAt(0);
+		int num = (int)a;
+		
+		System.out.println(num);
+	}
+
+}
