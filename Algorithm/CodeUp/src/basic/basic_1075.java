914fbf48702984b04c47a438cccdaa4dc70e9c81
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-03-06 21:05:28 +0900

    코드업 기초 100제

--- Algorithm/CodeUp/src/basic/basic_1075.java
+++ Algorithm/CodeUp/src/basic/basic_1075.java
@@ -2,21 +2,19 @@ package basic;
 
 import java.util.Scanner;
 
-public class basic_1073 {
+public class basic_1075 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		int n = sc.nextInt();
 		
-		while(n!=0) {
-			n = sc.nextInt();
-			
-			if(n==0) break;
-			System.out.println(n);
+		while (n!=0) {
+			for(int i = n-1; i >= 0; i--) {
+				System.out.println(i);
+			}
+			break;
 		}
-		
-		
-		
 	}
 
 }
