6bab5a4042908c30c2fa405569b9cd13e9ab44fa
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-04-09 11:58:03 +0900

    코드업 기초 100제

--- Algorithm/CodeUp/src/basic/basic_1096.java
+++ Algorithm/CodeUp/src/basic/basic_1096.java
@@ -1,8 +1,26 @@
 package basic;
 
+import java.util.Scanner;
+
 public class basic_1096 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
+		int arr[][] = new int[19][19];
+		
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			arr[a-1][b-1] = 1;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
