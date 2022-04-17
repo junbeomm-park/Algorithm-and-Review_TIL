a8e121f0948ca929065e658766269dcb80ac82d3
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-04-17 14:00:33 +0900

    코드업 기초 100제

--- Algorithm/CodeUp/src/basic/basic_1098.java
+++ Algorithm/CodeUp/src/basic/basic_1098.java
@@ -6,16 +6,33 @@ public class basic_1098 {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int h = sc.nextInt();
-		int w = sc.nextInt();
-		int d = sc.nextInt();
-		int arr[][] = new int[h][w];
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+		int arr[][] = new int[H+1][W+1];
+		int N = sc.nextInt();
 		
-		for(int i = 0; i < arr.length; i++) {
-			for(int j = 0; j < arr.length; j++) {
-				arr[i][j] = 0;
+		for(int i = 0; i < N; i++) {
+			int L = sc.nextInt();
+			int D = sc.nextInt();
+			int X = sc.nextInt();
+			int Y = sc.nextInt();
+			
+			if(D==0) {
+				for(int j = 0; j < L; j++) {
+					arr[X][Y+j] = 1;
+				}
 				
-				
+			}else if(D==1) {
+				for(int j = 0; j < L; j++) {
+					arr[X+j][Y] = 1;
+				}
+			}
+			
+		}
+		
+		
+		for(int i = 1; i <= H; i++) {
+			for(int j = 1; j <= W; j++) {
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
