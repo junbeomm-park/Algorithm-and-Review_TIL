6b9970eb11c3b804d7d000a6d02c963660fa2967
junbeomm-park <88473491+junbeomm-park@users.noreply.github.com>
2022-04-24 20:20:58 +0900

    코드업 기초 100제

---
+++ Algorithm/CodeUp/src/basic/basic_1036.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.Scanner;
+
+public class basic_1036 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		
+		System.out.println((int)c);
+	}
+
+}
